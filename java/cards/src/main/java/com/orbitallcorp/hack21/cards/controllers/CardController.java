91baabe940888b803c47e723ec771abdb8e1bc5c
unknown <cavalinhobelga@outlook.com>
2021-01-23 12:12:46 -0300

    projetojava

---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
@@ -0,0 +1,73 @@
+package com.orbitallcorp.hack21.cards.controllers;
+
+import com.orbitallcorp.hack21.cards.domains.Card;
+import com.orbitallcorp.hack21.cards.services.CardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.message.callback.SecretKeyCallback;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cards")
+public class CardController {
+    private CardService cardServi;
+
+    //Retorna todos os cartões
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    @GetMapping
+    public ResponseEntity<List<Card>> getCards(){
+        return ResponseEntity.ok(cardServi.getCards());
+    }
+
+    //Insere um registro na tabela card do banco de dados
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Card setCard(@RequestBody Card card){
+        return cardServi.setCard(card);
+    }
+
+    //Atualiza um registro existente
+
+    @PutMapping(path = "/cards/{id}")
+    public ResponseEntity<Card> updateCard(@PathVariable(name="id") Long id,@RequestBody Card card){
+        Card newCard =  cardServi.updateCard(id,card);
+
+       if(newCard==null){
+            return new ResponseEntity(newCard,HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity(newCard,HttpStatus.OK);
+        }
+    }
+
+    //Deleta um cartão por id
+    @DeleteMapping(path = "/cards/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<String> deleteCard(@PathVariable(name="id") Long id){
+        boolean newCard =  cardServi.deleteCard(id);
+
+        if(newCard){
+            return new ResponseEntity("Deletado com sucesso",HttpStatus.OK);
+        }else{
+            return new ResponseEntity("Não existe um cadastro com esse id",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Retorna um cartão específico
+    @GetMapping(path = "/cards/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity getCard(@PathVariable(name="id") Long id){
+        boolean newCard =  cardServi.deleteCard(id);
+
+        if(newCard){
+            return new ResponseEntity(newCard,HttpStatus.OK);
+        }else{
+            return new ResponseEntity("Não existe um cadastro com esse id",HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+}
---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/services/CardService.java
@@ -0,0 +1,65 @@
+package com.orbitallcorp.hack21.cards.services;
+
+import com.orbitallcorp.hack21.cards.domains.Card;
+import com.orbitallcorp.hack21.cards.repositories.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CardService{
+
+    @Autowired
+    private CardRepository cardRepo;
+
+    //Retorna todos os cartões
+    public List<Card> getCards(){
+        return (List<Card>) cardRepo.findAll();
+    }
+
+    //Insere um registro na tabela card do banco de dados
+    public Card setCard(Card card){
+        return cardRepo.save(card);
+    }
+
+    //Atualiza um registro existente
+    public Card updateCard(Long id,Card cardPostUpdate){
+        List<Card> cards = (List<Card>) cardRepo.findAll();
+
+        for(Card c: cards){
+            if(c.getId().equals(id)){
+                return cardRepo.save(cardPostUpdate);
+            }
+        }
+        return null;
+    }
+
+    //Deleta um cartão por id
+    public boolean deleteCard(Long id){
+        Optional<Card> card = cardRepo.findById(id);
+
+        if(card.isPresent()) {
+            cardRepo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    //Retorna um cartão específico
+    public Card getCard(Long id){
+        List<Card> cards = (List<Card>) cardRepo.findAll();
+
+        for(Card c: cards){
+            if(c.getId().equals(id)){
+                return c;
+            }
+        }
+        return null;
+
+
+    }
+
+}
