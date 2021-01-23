52c900655f151307c5d6f122373c4cfaaae13d01
unknown <cavalinhobelga@outlook.com>
2021-01-23 15:10:23 -0300

    nodejs

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,6 +2,120 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    let data = neDB.find({},request.body,function(exception,docs){
+        let message = docs;
+        let status = 200;
+
+        if (exception){
+            message = {"Houve um erro ao coletar dados: ":exception};
+            status = 400
+        }
+
+        response.status(status);
+        response.json(message);
+
+
+    })
+
 }
+
+api.insert = (request, response) => {
+    let insert = neDB.insert(request.body,function(exception,docs){
+        let message = docs;
+        let status = 201;
+
+        if (exception){
+            message = {"Houve um erro ao inserir dados: ":exception};
+            status = 400
+        }
+
+        response.status(status);
+        response.json(message);
+
+
+    })
+
+}
+
+api.update = (request, response) => {
+    let id = request.params.id;
+
+    let update = neDB.update({_id:id},{ $set: request.body},{multi:true},function(exception,numreplaced){
+        let message = {"Sucesso": "Um registro foi alterado com sucesso"};
+        let status = 200;
+
+        if (exception){
+            message = {"Houve um erro ao atualizar dados: ":exception};
+            status = 400
+        }
+
+        response.status(status);
+        response.json(message);
+
+
+    })
+
+}
+
+api.delete = (request, response) => {
+    let id = request.params.id;
+
+    let remove = neDB.remove({_id:id},{},function(exception,numdeleted){
+        let message = {"Sucesso": "Um registro foi deletado com sucesso"};
+        let status = 200;
+
+        if (exception){
+            message = {"Houve um erro ao deletar dados: ":exception};
+            status = 400
+        }
+
+        response.status(status);
+        response.json(message);
+
+
+    })
+    
+}
+
+api.get = (request, response) => {
+    let id = request.params.id;
+
+    let data = neDB.find({_id:id},{},function(exception,docs){
+        let message = docs;
+        let status = 200;
+
+        if (exception){
+            message = {"Houve um erro ao coletar dados individuais: ":exception};
+            status = 400
+        }
+
+        response.status(status);
+        response.json(message);
+
+
+    })
+    
+    
+}
+
+api.pagination = (request, response) => {
+        let data = neDB.find({}).sort({customerName:1}).skip(1).limit(1).exec( (exception, docs) => {
+            let message = docs;
+            let status = 200;
+
+            if (exception){
+                message = {"Houve um erro ao encontrar a página desejada: ":exception};
+                status = 400
+            }
+
+            response.status(status);
+            response.json(message);
+
+
+      })
+    
+}
+
+
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,11 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
-
+        .post(api.insert);
+        
+    app.route("/cards/:id").put(api.update).delete(api.delete).get(api.get);
+    app.route("/cards/paginationAndSorting").get(api.pagination);
+        
 }
