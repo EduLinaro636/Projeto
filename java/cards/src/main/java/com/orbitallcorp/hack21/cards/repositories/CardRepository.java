e2a9843dfaa644c4034aa1cd09321fd73f089d07
unknown <cavalinhobelga@outlook.com>
2021-01-23 12:17:31 -0300

    projetojava

--- java/cards/src/main/java/com/orbitallcorp/hack21/cards/repositories/CardRepository.java
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/repositories/CardRepository.java
@@ -3,6 +3,6 @@ package com.orbitallcorp.hack21.cards.repositories;
 import com.orbitallcorp.hack21.cards.domains.Card;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CardRepository<Card,Long> extends PagingAndSortingRepository<Card, Long> {
+public interface CardRepository extends PagingAndSortingRepository<Card, Long> {
 
 }
